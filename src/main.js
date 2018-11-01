737a215c51897fa4971640901d27c40cfacd9319
boy-wy <15010519351@163.com>
2018-11-01 17:17:50 +0800

    登录页完成

--- src/main.js
+++ src/main.js
@@ -2,9 +2,18 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import HttpTool from '@/plugins/http.js'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/assets/index.css'
 import router from './router'
 
 Vue.config.productionTip = false
+
+Vue.use(HttpTool)
+Vue.use(ElementUI)
+
+
 
 /* eslint-disable no-new */
 new Vue({
